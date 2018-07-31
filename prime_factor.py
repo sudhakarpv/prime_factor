144b98fd705252d022d6d98d0921a1e6ea4effc7
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-31 12:21:36 +0530

    Update and rename README.md to prime_factor.py

---
+++ prime_factor.py
@@ -0,0 +1,19 @@
+# prime_factor
+def main():
+    pass
+    a=int(input())
+    l=[]
+    list_2=[]
+    for num in range (2,a):
+        if (num>1):
+            for i in range(2,num):
+                if (num%i==0):
+                    break
+            else:
+                l.append(num)
+    for k in l:
+        if(a%k==0):
+            list_2.append(k)
+    print(' '.join(map(str,list_2)))
+if __name__ == '__main__':
+    main()

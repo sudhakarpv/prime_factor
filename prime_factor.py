89f83e78c7073478ad96cb662207243acf990819
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-31 12:37:48 +0530

    Update prime_factor.py

--- prime_factor.py
+++ prime_factor.py
@@ -4,7 +4,7 @@ def main():
     a=int(input())
     l=[]
     list_2=[]
-    for num in range (2,a):
+    for num in range (2,a+1):
         if (num>1):
             for i in range(2,num):
                 if (num%i==0):
@@ -15,5 +15,6 @@ def main():
         if(a%k==0):
             list_2.append(k)
     print(' '.join(map(str,list_2)))
+
 if __name__ == '__main__':
     main()
